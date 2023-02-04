08c3cfce0d916fb959e18735e443b3171a38dc81
AngelinaTomashkevych <porucnikangelina@gmail.com>
2023-02-04 14:43:43 +0200

    development: add express Router

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const Router = require('./Router');
+
+const corsMiddleware = cors();
 
 const app = express();
+
+app.use(corsMiddleware);
 
 app.use((request, response, next) => {
     next();
 });
 
-app.get('/tasks', (request, response) => {
-    response.json({ message: 'success' });
+app.use('/app', Router);
+
+app.use((_, response) => {
+    response.status(404).json({ message: 'Not found' });
+});
+
+app.use((error, _, response, __) => {
+    response.status(500).json({ message: error.message });
 });
 
 app.listen(3000);
